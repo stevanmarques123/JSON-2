7c50c8a7f47bcb65a366ea840d7b357ab06ccf98
stevanmarques123 <stevan.marques@escola.pr.gov.br>
2026-04-29 11:47:04 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-function busca_alunos(){
-fetch('dados.json')
-.then(response => response.json())
-.then(ddd => {
-    tablet = document.getElementById('lugar');
+function buscar_alunos(){
+  fetch('dados.json')
+     .then(response => response.json())
+     .then(ddd => {
+
+    banana = document.getElementById('lugar');
+
     ddd.forEach(pessooainha =>{
-        let linha = document.createElement('tr')
+        let linha = document.createElement('tr');
+
         linha.innerHTML=
         "<td>" +pessooainha.nome + "</td>" +
         "<td>" +pessooainha.idade + "</td>" +
         "<td>" +pessooainha.personalidade + "</td>";
         tablet.appendchild(linha);
-
     })
-})
+});
 
 
 }
